3d1ac99825245b4d1e27e9f5870e982c92ca1f63
Franck-Fernandez-pro <franck.fernandez@gmail.com>
2019-01-16 10:31:14 +0100

    Availability Entity and Repository created

---
+++ src/Entity/Availability.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AvailabilityRepository")
+ */
+class Availability
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $id_property;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdProperty(): ?int
+    {
+        return $this->id_property;
+    }
+
+    public function setIdProperty(int $id_property): self
+    {
+        $this->id_property = $id_property;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
