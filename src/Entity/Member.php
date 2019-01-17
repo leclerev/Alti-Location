933a77013e5e9bfd871d0dd992a2cd37ab2b36d0
A.Baptiste <baptiste@skwr.io>
2019-01-17 13:29:55 +0100

    patched conflict

--- src/Entity/Member.php
+++ src/Entity/Member.php
@@ -19,16 +19,6 @@ class Member
     private $id;
 
     /**
-     * @ORM\OneToMany(targetEntity="App\Entity\Property", mappedBy="member")
-     */
-    private $property;
-
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\Advert", mappedBy="author")
-     */
-    private $advert;
-
-    /**
      * @ORM\Column(type="string", length=255)
      */
     private $name;
@@ -58,78 +48,20 @@ class Member
      */
     private $opinions;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Property", mappedBy="author", orphanRemoval=true)
+     */
+    private $properties;
+
     public function __construct()
     {
-        $this->property = new ArrayCollection();
-        $this->advert = new ArrayCollection();
         $this->opinions = new ArrayCollection();
+        $this->properties = new ArrayCollection();
     }
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    /**
-     * @return Collection|Property[]
-     */
-    public function getProperty(): Collection
-    {
-        return $this->property;
-    }
-
-    public function addProperty(Property $property): self
-    {
-        if (!$this->property->contains($property)) {
-            $this->property[] = $property;
-            $property->setMember($this);
-        }
-
-        return $this;
-    }
-
-    public function removeProperty(Property $property): self
-    {
-        if ($this->property->contains($property)) {
-            $this->property->removeElement($property);
-            // set the owning side to null (unless already changed)
-            if ($property->getMember() === $this) {
-                $property->setMember(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Advert[]
-     */
-    public function getAdvert(): Collection
-    {
-        return $this->advert;
-    }
-
-    public function addAdvert(Advert $advert): self
-    {
-        if (!$this->advert->contains($advert)) {
-            $this->advert[] = $advert;
-            $advert->setAuthor($this);
-        }
-
-        return $this;
-    }
-
-    public function removeAdvert(Advert $advert): self
-    {
-        if ($this->advert->contains($advert)) {
-            $this->advert->removeElement($advert);
-            // set the owning side to null (unless already changed)
-            if ($advert->getAuthor() === $this) {
-                $advert->setAuthor(null);
-            }
-        }
-
-        return $this;
     }
 
     public function getName(): ?string
@@ -217,6 +149,37 @@ class Member
             // set the owning side to null (unless already changed)
             if ($opinion->getAuthor() === $this) {
                 $opinion->setAuthor(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Property[]
+     */
+    public function getProperties(): Collection
+    {
+        return $this->properties;
+    }
+
+    public function addProperty(Property $property): self
+    {
+        if (!$this->properties->contains($property)) {
+            $this->properties[] = $property;
+            $property->setAuthor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProperty(Property $property): self
+    {
+        if ($this->properties->contains($property)) {
+            $this->properties->removeElement($property);
+            // set the owning side to null (unless already changed)
+            if ($property->getAuthor() === $this) {
+                $property->setAuthor(null);
             }
         }
 
