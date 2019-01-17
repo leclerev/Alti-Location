5ac4f2418639e2854686062aca5ff333be90097b
A.Baptiste <baptiste@skwr.io>
2019-01-17 13:22:35 +0100

    added an homepage controller

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/default", name="default")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route("/colo", name="default")
+     */
+    public function colo()
+    {
+        return $this->render('default/colo.html.twig', [
+        ]);
+    }
+}
+
+
